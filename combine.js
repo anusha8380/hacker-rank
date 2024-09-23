d8bbc0821afd0495b94661852f2b4aa6ace0bfea
anusha8380 <94095631+anusha8380@users.noreply.github.com>
2024-09-23 15:49:20 +0530

    Create combine.js

---
+++ combine.js
@@ -0,0 +1,19 @@
+var combine = function(n, k) {
+    //Write Code here
+    let res = [];
+    function helper(start,curr){
+        if(curr.length === k){
+            res.push([...curr]);
+            return
+        }
+        for(let j = start; j<= n;j++){
+            curr.push(j);
+            helper(j+1,curr);
+            curr.pop()
+        }
+    }
+    
+    helper(1,[])
+     return res;
+    
+};
